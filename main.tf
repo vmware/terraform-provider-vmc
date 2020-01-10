0223e5aa3f39bef28c819cf4bd2b4957b9794732
shylasrinivas <58704937+shylasrinivas@users.noreply.github.com>
2020-01-10 09:08:50 -0800

    Changes to *.tf and readme in VMC provider for compatibility with terraform v0.12 (#12)
    * IAC-70 changes in tf file for terraform v0.12 compatibility
    
    * IAC-70 changes in *.tf file and readme for terraform v0.12 compatibility

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@ data "vmc_org" "my_org" {
 }
 
 data "vmc_connected_accounts" "my_accounts" {
-  org_id = "${data.vmc_org.my_org.id}"
+  org_id = data.vmc_org.my_org.id
 }
 
 data "vmc_customer_subnets" "my_subnets" {
-  org_id               = "${data.vmc_org.my_org.id}"
-  connected_account_id = "${data.vmc_connected_accounts.my_accounts.ids.0}"
+  org_id               = data.vmc_org.my_org.id
+  connected_account_id = data.vmc_connected_accounts.my_accounts.ids[0]
   region               = "US_WEST_2"
 }
 
 resource "vmc_sddc" "sddc_1" {
-  org_id = "${data.vmc_org.my_org.id}"
+  org_id = data.vmc_org.my_org.id
 
   # storage_capacity    = 100
   sddc_name           = ""
   vpc_cidr            = "10.2.0.0/16"
   num_host            = 1
   provider_type       = "AWS"
-  region              = "${data.vmc_customer_subnets.my_subnets.region}"
+  region              = data.vmc_customer_subnets.my_subnets.region
   vxlan_subnet        = "192.168.1.0/24"
   delay_account_link  = false
   skip_creating_vxlan = false
@@ -33,12 +33,10 @@ resource "vmc_sddc" "sddc_1" {
   # sddc_template_id = ""
   deployment_type = "SingleAZ"
 
-  account_link_sddc_config = [
-    {
-      customer_subnet_ids  = ["${data.vmc_customer_subnets.my_subnets.ids.0}"]
-      connected_account_id = "${data.vmc_connected_accounts.my_accounts.ids.0}"
-    },
-  ]
+  account_link_sddc_config {
+    customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
+    connected_account_id = data.vmc_connected_accounts.my_accounts.ids[0]
+  }
   timeouts {
     create = "300m"
     update = "300m"
@@ -47,9 +45,9 @@ resource "vmc_sddc" "sddc_1" {
 }
 
 resource "vmc_publicips" "IP1" {
-  org_id = "${data.vmc_org.my_org.id}"
-  sddc_id = "${vmc_sddc.sddc_1.id}"
+  org_id     = data.vmc_org.my_org.id
+  sddc_id    = vmc_sddc.sddc_1.id
   private_ip = "10.2.33.45"
-  name = "vm1"
+  name       = "vm1"
 }
 
