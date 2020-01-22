7d8a41524f2298da3512ac35d7e852d0cf6f577b
shylasrinivas <58704937+shylasrinivas@users.noreply.github.com>
2020-01-21 16:06:22 -0800

    Moved main.tf to examples/ (#17)
    * IAC-70 changes in tf file for terraform v0.12 compatibility
    
    * IAC-70 changes in *.tf file and readme for terraform v0.12 compatibility
    
    * Moved main.tf to examples/
    
    * improved description for variables
    
    * removed commented variables

--- examples/variables.tf
+++ examples/variables.tf
@@ -1,15 +1,15 @@
 variable "sddc_region" {
-  description = "The AWS region to create things in."
+  description = "The AWS region."
   default     = "US_WEST_2"
 }
 
 variable "vpc_cidr" {
-  description = "The cidr for VPC."
+  description = "AWS VPC IP range. Only prefix of 16 or 20 is currently supported."
   default     = "10.2.0.0/16"
 }
 
 variable "vxlan_subnet" {
-  description = "The subnet of SDDC."
+  description = "VXLAN IP subnet in CIDR for compute gateway."
   default     = "192.168.1.0/24"
 }
 
