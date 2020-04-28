5d053b20f048f30dfcf1c9bc259e7fdf256e032e
Salonee Rege <srege@vmware.com>
2020-04-28 12:12:49 -0700

    Added separate folders for each resource in examples folder.

---
+++ examples/public_ip/main.tf
@@ -0,0 +1,50 @@
+provider "vmc" {
+  refresh_token = var.api_token
+  org_id = var.org_id
+}
+
+# Empty data source defined in order to store the org display name and name in terraform state
+data "vmc_org" "my_org" {
+}
+
+data "vmc_connected_accounts" "my_accounts" {
+  account_number = var.aws_account_number
+}
+
+data "vmc_customer_subnets" "my_subnets" {
+  connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  region               = replace(upper(var.sddc_region), "-", "_")
+}
+
+resource "vmc_sddc" "sddc_1" {
+  sddc_name           = var.sddc_name
+  vpc_cidr            = var.vpc_cidr
+  num_host            = var.num_hosts
+  provider_type       = var.provider_type
+  region              = data.vmc_customer_subnets.my_subnets.region
+  vxlan_subnet        = var.vxlan_subnet
+  delay_account_link  = false
+  skip_creating_vxlan = false
+  sso_domain          = "vmc.local"
+  sddc_type = var.sddc_type
+  deployment_type = "SingleAZ"
+
+  host_instance_type = var.host_instance_type
+
+  storage_capacity = var.storage_capacity
+
+  account_link_sddc_config {
+    customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
+    connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  }
+  timeouts {
+    create = "300m"
+    update = "300m"
+    delete = "180m"
+  }
+}
+
+resource "vmc_public_ip" "public_ip_1" {
+  nsxt_reverse_proxy_url = vmc_sddc.sddc_1.nsxt_reverse_proxy_url
+  display_name = var.public_ip_displayname
+}
