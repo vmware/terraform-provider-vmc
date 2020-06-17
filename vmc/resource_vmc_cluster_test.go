d014648d1b3a8a9aa491d7ff446f6e1af71eec2d
Salonee Rege <srege@vmware.com>
2020-06-17 14:40:50 -0700

    Updated the cluster basic config method

--- vmc/resource_vmc_cluster_test.go
+++ vmc/resource_vmc_cluster_test.go
@@ -5,6 +5,7 @@ package vmc
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
 	"os"
 	"strings"
 	"testing"
@@ -18,13 +19,14 @@ import (
 
 func TestAccResourceVmcCluster_basic(t *testing.T) {
 	var sddcResource model.Sddc
+	sddcName := "terraform_test_sddc_" + acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testCheckVmcClusterDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVmcClusterConfigBasic(),
+				Config: testAccVmcClusterConfigBasic(sddcName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckVmcClusterExists("vmc_cluster.cluster_1", &sddcResource),
 					resource.TestCheckResourceAttrSet("vmc_cluster.cluster_1", "id"),
@@ -57,14 +59,15 @@ func testAccCheckVmcClusterExists(name string, sddcResource *model.Sddc) resourc
 		clusterExists := false
 		for i := 0; i < len(sddcResource.ResourceConfig.Clusters); i++ {
 			currentResourceConfig := sddcResource.ResourceConfig.Clusters[i]
-			if !strings.Contains(*currentResourceConfig.ClusterName, "Cluster-1") {
+			if strings.Contains(*currentResourceConfig.ClusterName, "Cluster-1") {
+				fmt.Printf("Cluster Name : %s", *currentResourceConfig.ClusterName)
 				clusterExists = true
 				break
 			}
 		}
 
 		if clusterExists != true {
-			return fmt.Errorf("error retrieving Cluster : %v", err)
+			return fmt.Errorf("error retrieving cluster : %v", err)
 		}
 
 		fmt.Printf("Cluster for SDDC %s created successfully \n", sddcID)
@@ -91,10 +94,12 @@ func testCheckVmcClusterDestroy(s *terraform.State) error {
 		for i := 0; i < len(sddcResource.ResourceConfig.Clusters); i++ {
 			currentResourceConfig := sddcResource.ResourceConfig.Clusters[i]
 			if !strings.Contains(*currentResourceConfig.ClusterName, "Cluster-1") {
+				fmt.Printf("Cluster Name : %s", *currentResourceConfig.ClusterName)
 				return fmt.Errorf("cluster still exists : %v", err)
 
 			}
 		}
+
 		//check if error type if not_found
 		if err.Error() != (errors.NotFound{}.Error()) {
 			return err
@@ -104,14 +109,46 @@ func testCheckVmcClusterDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAccVmcClusterConfigBasic() string {
+func testAccVmcClusterConfigBasic(sddcName string) string {
 	return fmt.Sprintf(`
+data "vmc_connected_accounts" "my_accounts" {
+      account_number = %q
+}
 
+data "vmc_customer_subnets" "my_subnets" {
+  connected_account_id = data.vmc_connected_accounts.my_accounts.id
+  region               = "US_WEST_2"
+}
+resource "vmc_sddc" "sddc_1" {
+	sddc_name = %q
+	vpc_cidr      = "10.2.0.0/16"
+	num_host      = 3
+	provider_type = "AWS"
+
+	region = "US_WEST_2"
+
+	vxlan_subnet = "192.168.1.0/24"
+	delay_account_link  = false
+	skip_creating_vxlan = false
+	sso_domain          = "vmc.local"
+	deployment_type = "SingleAZ"
+    account_link_sddc_config {
+    customer_subnet_ids  = [data.vmc_customer_subnets.my_subnets.ids[0]]
+    connected_account_id = data.vmc_connected_accounts.my_accounts.id
+    }
+    timeouts {
+      create = "300m"
+      update = "300m"
+      delete = "180m"
+  }
+}
 resource "vmc_cluster" "cluster_1" {
-	sddc_id = %q
+	sddc_id = vmc_sddc.sddc_1.id
 	num_hosts      = 3
     }
+
 `,
-		os.Getenv(TestSDDCId),
+		os.Getenv(AWSAccountNumber),
+		sddcName,
 	)
 }
