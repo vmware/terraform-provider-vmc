28d43c7313ac028f5e0267a39af4ad4db3fbaa7f
Dimitar Proynov <proynovd@vmware.com>
2023-01-06 11:34:39 +0200

    Increase SDDC Groups timeouts due to slow backend, that fails the E2E tests
    See run: https://github.com/vmware/terraform-provider-vmc/actions/runs/3853633817/jobs/6566794901
    
    The SDDC Groups test has failed at 1800s, which is the current "Create" timeout. The Network Connectivity Group still in pending state as the  backend is probably busy.
    
    Also update changelog.md with concrete date for last release.
    
    Testing done:
    make build
    make test
    golangci-lint run
    
    Signed-off-by: Dimitar Proynov <proynovd@vmware.com>

--- vmc/resource_vmc_sddc_group.go
+++ vmc/resource_vmc_sddc_group.go
@@ -28,9 +28,9 @@ func resourceSddcGroup() *schema.Resource {
 			StateContext: schema.ImportStatePassthroughContext,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(20 * time.Minute),
-			Update: schema.DefaultTimeout(10 * time.Minute),
+			Create: schema.DefaultTimeout(60 * time.Minute),
+			Delete: schema.DefaultTimeout(30 * time.Minute),
+			Update: schema.DefaultTimeout(30 * time.Minute),
 		},
 	}
 }
